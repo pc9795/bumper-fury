d33ee32bca0df018a81dadd3a27dd57c57a1ea94
Prashant <prashantchaubey9795@gmail.com>
2020-04-06 00:33:16 +0100

    Implemented mini map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,10 @@ public class CameraController : MonoBehaviour
 {
     //Public fields
     public GameObject toFollow;
-    
-    //Private fields
-    private Vector3 offset;
 
     //Unity Methods
-    void Start()
-    {
-        offset = transform.position - toFollow.transform.position;
-
-    }
-
     void LateUpdate()
     {
-        transform.position = toFollow.transform.position + offset;
+        transform.LookAt(toFollow.transform);
     }
 }
---
+++ Assets/Scripts/MiniMapController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MiniMapController : MonoBehaviour
+{
+    //Public fields
+    public Transform toFollow;
+
+    //Unity methods
+    void LateUpdate()
+    {
+        Vector3 newPosition = toFollow.position;
+        //Keep the zoom level inact.
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}

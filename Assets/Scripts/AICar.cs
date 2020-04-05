0f14f0da1d3e9a004d7576fe4d16abb1a5df8f81
Prashant <prashantchaubey9795@gmail.com>
2020-04-05 20:22:01 +0100

    Added shock wave effect on projectiles

---
+++ Assets/Scripts/AICar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AICar : MonoBehaviour
+{
+    //Public ones
+    public string id;
+    //Private ones
+    private Rigidbody _rigidbody;
+    private int _score;
+    private int _health;
+    //Getters and Setters
+    new public Rigidbody rigidbody { get { return _rigidbody; } }
+    public int score { get { return _score; } }
+    public int health { get { return _health; } }
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ModelContainer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ModelContainer : MonoBehaviour
+{
+    public GameObject model;
+    private GameObject modelInstance;
+    private Rigidbody parentRigidBody;
+    private float groundLevel = 0.6f;
+    void Start()
+    {
+        // Its parent should be a rigid body.
+        parentRigidBody = transform.parent.GetComponent<Rigidbody>();
+        Vector3 position = parentRigidBody.transform.position;
+        position.y = groundLevel;
+        modelInstance = Instantiate(model, position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        modelInstance.transform.position = transform.position;
+    }
+}

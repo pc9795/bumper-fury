817d301c49be3a1580df7b390760a0de759a4589
Prashant <prashantchaubey9795@gmail.com>
2020-04-06 17:00:41 +0100

    added nitros

---
+++ Assets/GameFiles/Scripts/NitroBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class NitroBehavior : MonoBehaviour
+{
+    //Public fields
+    public GameObject nitroFlame;
+    public int flameDuration;
+
+    //Private fields
+    private Rigidbody rigidBody;
+    private int incrmentSize;
+    private GameObject nitroFlameInstance;
+
+    //Unity methods
+    void Start()
+    {
+        rigidBody = GetComponentInParent<Rigidbody>();
+        incrmentSize = 5;
+    }
+
+    void Update()
+    {
+        //Get the integer part. It will be easy then.
+        int speed = (int)rigidBody.velocity.magnitude;
+        if (speed != 0 && speed % incrmentSize == 0 && !nitroFlameInstance)
+        {
+            nitroFlameInstance = Instantiate(nitroFlame, transform.position, Quaternion.identity);
+            Destroy(nitroFlameInstance, flameDuration);
+        }
+        if (nitroFlameInstance)
+        {
+            nitroFlameInstance.transform.position = transform.position;
+            nitroFlameInstance.transform.forward = transform.forward;
+        }
+    }
+}

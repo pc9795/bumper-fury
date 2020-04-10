c986ff649fb510cca3a2f04d0d96c3e58a4ed6ee
Prashant <prashantchaubey9795@gmail.com>
2020-04-10 16:26:19 +0100

    Added main menu scene

---
+++ Assets/GameFiles/Scripts/UI/Main menu/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Story()
+    {
+
+    }
+
+    public void Battle()
+    {
+
+    }
+
+    public void Help()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/GameFiles/Scripts/UI/Main menu/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, -0.5f, 0), Space.Self);
+    }
+}

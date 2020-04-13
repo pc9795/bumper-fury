076ef8091f1196978f0b80572f545cdd5f3df9ab
Prashant <prashantchaubey9795@gmail.com>
2020-04-14 00:57:11 +0100

    Implemented earth level

--- Assets/GameFiles/Scripts/Levels/EarthLevel.cs
+++ Assets/GameFiles/Scripts/Levels/EarthLevel.cs
@@ -2,15 +2,8 @@
 
 public class EarthLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameManager.INSTANCE.InitLevel();
     }
 }

c597c228c1cb86fa6c295a1b67087e77a36b4d53
Prashant <prashantchaubey9795@gmail.com>
2020-04-25 11:51:14 +0100

    Updated docs for NPC

--- Assets/GameFiles/Scripts/Entities/NPC/AICar.cs
+++ Assets/GameFiles/Scripts/Entities/NPC/AICar.cs
@@ -4,54 +4,72 @@
 //I referenced about AI movements from the above mentioned youtube tutorial on Car AI.
 public class AICar : MonoBehaviour
 {
+    //Sensors to the car.
     public enum Sensor
     {
         LEFT, LEFT_ANGLE, RIGHT, RIGHT_ANGLE, CENTER, BACK
     }
 
     //Public fields
+
+    //GameObject which will be the 3d model of the car.
     public GameObject modelPrefab;
+    //Centre of the mass of the car.
     public Vector3 centreOfMass = new Vector3(0, 0.3f, 0);
+    //Top speed of the AI car.
     public int topSpeed = 20;
 
     // Private fields
-    //Many of the configurations can be moved to AI manager.
+
+    //`StatsController` for this car. It will be hadling statistics like health, enrgy, score etc.
     private StatsController stats;
+    //`SimpleCarController` for this car. It will control the underlying model. This script is not attached directly to this object
+    //It will be added to the `modelPrefab`.
     private SimpleCarController carController;
+    //It will be used to fire and manage projectiles.
     private ProjectileShooter projectileShooter;
+    //A car model is attached to this object dynamically therefore need a variable to keep a track of this.
     private bool initialized;
+    //Instance of the `modelPrefab`.
     private GameObject modelInstance;
+    //Keep track of the horizontal input to be applied to underlying car model. x direction input.
     private float horizontalInput;
+    //Keep track of the vertical input to be applied to underlying car model. y direction input.
     private float verticalInput;
+    //Current way point of this AI
     private AIManager.WayPoint currWaypoint;
+    //Rigid body attached to this AI
     new private Rigidbody rigidbody;
+    //A variable to keep track whether it is reversing or not.
     private bool reversing;
+    //Direction of reverse. This will be set while reversing to decide whether car is reversed or not.
     private Vector3 reverseDirection;
+    //Last time when speed was checked
     private float lastSpeedCheckTime;
+    //A flag to keep track whether it is flipped or not.
     private bool flipped;
+    //A flag to keep track whether the car was outside smart boundary in the last frame.
     private bool wasOutsideSmartBoundary;
-    private Scoreboard scoreboard;
+    //A flag to keep track whether an obstacle is detected by the sensors or not.
     private bool obstacleFound;
+    //An input needed to avoid the obstacle. x direction
     private float obstacleAvoidanceInput;
+    //A flag to keep track whether this AI is inside tornado or not.
     private bool insideTornado;
+    //What was the last time when a way point was reached
     private float lastWayPointTime;
 
     // Unity methods
     void Start()
     {
-        //Get this object AI tag.
+        //Get this object's AI tag.
         gameObject.tag = GameManager.Tag.AI;
     }
 
     void LateUpdate()
     {
-        //No updates if not initialized.
-        if (!initialized)
-        {
-            return;
-        }
-        //No updates if dead.
-        if (!stats.IsAlive())
+        //No updates if not initialized or dead.
+        if (!initialized || !stats.IsAlive())
         {
             return;
         }
@@ -63,9 +81,18 @@ public class AICar : MonoBehaviour
             stats.Die();
             AIManager.INSTANCE.NotifyDeath(stats.displayName);
         }
+        //Clear if there is some input from the previous sense iteration.
+        obstacleFound = false;
+        obstacleAvoidanceInput = 0f;
+        //Release handbrakes by default. If needed it will be set down the flow again.
         carController.ReleaseHandBrake();
         //AI Behaviors
         AI();
+        //If obstacle is found change the direction accordingly in x-direction.
+        if (obstacleFound)
+        {
+            horizontalInput = obstacleAvoidanceInput;
+        }
         //Move the car
         carController.Steer(horizontalInput);
         carController.Move(verticalInput);
@@ -73,6 +100,8 @@ public class AICar : MonoBehaviour
         //Collect any score done by the current projectile if exists.
         float damageDoneWithProjectile = projectileShooter.CollectDamageDone();
         stats.AddScore(GameManager.INSTANCE.GetScoreFromDamage(damageDoneWithProjectile));
+        //Turn this off by default. If needed it will be set down in the `OnTriggerStaty` again. This logic works with the assumption
+        //That `OnTriggerStay` is executed before `LateUpdate`.
         insideTornado = false;
     }
 
@@ -86,26 +115,14 @@ public class AICar : MonoBehaviour
 
     void OnTriggerStay(Collider collider)
     {
-        Tornado tornado = collider.GetComponent<Tornado>();
-        if (tornado != null)
+        if (CheckTornadoTrigger(collider))
         {
-            stats.DamageHealth(tornado.baseDamage);
-            insideTornado = true;
             return;
         }
-
-        Storm storm = collider.GetComponent<Storm>();
-        if (storm != null)
+        if (CheckStormTrigger(collider))
         {
-            rigidbody.AddForce(storm.transform.forward * (storm.force / 2));
-
-            if (rigidbody.velocity.magnitude > AIManager.INSTANCE.stormSpeed)
-            {
-                carController.ApplyHandBrake();
-            }
             return;
         }
-
         if (CheckLavaTrigger(collider))
         {
             return;
@@ -118,31 +135,8 @@ public class AICar : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag(GameManager.Tag.ROCK))
+        if (CheckRockCollision(collision))
         {
-            foreach (ContactPoint contact in collision.contacts)
-            {
-                if (Mathf.Abs(Vector3.Dot(transform.up, contact.normal)) > 0.90)
-                {
-                    //Rock will have a rigid body
-                    float mass = collision.collider.GetComponent<Rigidbody>().mass;
-                    float relativeVelocity = collision.relativeVelocity.magnitude;
-
-                    //Found using experimentaion.
-                    float powerFactor = 50;
-                    float maxForce = 200000;
-                    float radius = 40;
-                    float upwardRift = 2;
-
-                    //When weight of the rock is 1000 and then this value is coming in the range of approx 1000-15000
-                    //If the value of rock weight change then update this accordingly.
-                    float damage = (mass * relativeVelocity) / 1000;
-                    float force = Mathf.Clamp(mass * relativeVelocity * powerFactor, 0, maxForce);
-                    stats.DamageHealth(damage);
-                    rigidbody.AddExplosionForce(force, transform.position, radius, upwardRift);
-                    break;
-                }
-            }
             return;
         }
         if (CheckPlayerCollision(collision))
@@ -177,8 +171,6 @@ public class AICar : MonoBehaviour
         carController = modelInstance.GetComponent<SimpleCarController>();
         carController.maxSteerAngle = AIManager.INSTANCE.maxSteerAngle;
         carController.motorForce = AIManager.INSTANCE.motorForce;
-        //Initialize the scoreboard.
-        scoreboard = FindObjectOfType<Scoreboard>();
 
         lastSpeedCheckTime = Time.time;
         //Let the script now that updates can be applied on this object as it is initialized.
@@ -228,6 +220,35 @@ public class AICar : MonoBehaviour
         return true;
     }
 
+    private bool CheckTornadoTrigger(Collider collider)
+    {
+        Tornado tornado = collider.GetComponent<Tornado>();
+        if (tornado == null)
+        {
+            return false;
+        }
+        stats.DamageHealth(tornado.baseDamage);
+        insideTornado = true;
+        return true;
+    }
+
+    private bool CheckStormTrigger(Collider collider)
+    {
+        Storm storm = collider.GetComponent<Storm>();
+        if (storm == null)
+        {
+            return false;
+        }
+        //Decrease storm force as AI is not very good at manuvering.
+        rigidbody.AddForce(storm.transform.forward * (storm.force / 2));
+        //Slow down the AI in storm.
+        if (rigidbody.velocity.magnitude > AIManager.INSTANCE.stormSpeed)
+        {
+            carController.ApplyHandBrake();
+        }
+        return true;
+    }
+
     private bool CheckLavaTrigger(Collider collider)
     {
         Lava lava = collider.GetComponent<Lava>();
@@ -248,6 +269,43 @@ public class AICar : MonoBehaviour
         }
         stats.DamageHealth(water.baseDamage);
         return true;
+    }
+
+    private bool CheckRockCollision(Collision collision)
+    {
+        if (!collision.collider.CompareTag(GameManager.Tag.ROCK))
+        {
+            return false;
+        }
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            //Make it configurable in future.
+            //The rock should fall right on the head.
+            if (Mathf.Abs(Vector3.Dot(transform.up, contact.normal)) < 0.90)
+            {
+                continue;
+            }
+            //Rock will have a rigid body
+            float mass = collision.collider.GetComponent<Rigidbody>().mass;
+            float relativeVelocity = collision.relativeVelocity.magnitude;
+
+            //Found using experimentaion.
+            float powerFactor = 50;
+            float maxForce = 200000;
+            float radius = 40;
+            float upwardRift = 2;
+
+            //When weight of the rock is 1000 and then this value is coming in the range of approx 1000-15000
+            //If the value of rock weight change then update this accordingly.
+            float damage = (mass * relativeVelocity) / 1000;
+            float force = Mathf.Clamp(mass * relativeVelocity * powerFactor, 0, maxForce);
+
+            stats.DamageHealth(damage);
+            rigidbody.AddExplosionForce(force, transform.position, radius, upwardRift);
+
+            return true;
+        }
+        return false;
     }
 
     private bool CheckPlayerCollision(Collision collision)
@@ -283,13 +341,16 @@ public class AICar : MonoBehaviour
         {
             return;
         }
+        //If below some velocity then not counted as an impacting collision
         if (collision.relativeVelocity.magnitude <= PhysicsManager.INSTANCE.collisionRelativeVelocityThreshold)
         {
             return;
         }
+        //Check whether collision was performed by this AI or another contestant
         bool performedBySelf = false;
         foreach (ContactPoint point in collision.contacts)
         {
+            //If the another contestant is in front of this car then damage is done by it.
             Vector3 relative = transform.InverseTransformPoint(point.point);
             if (relative.z >= PhysicsManager.INSTANCE.collisionRelativeVectorZThreshold)
             {
@@ -298,8 +359,11 @@ public class AICar : MonoBehaviour
             }
         }
 
-        float damage = GameManager.INSTANCE.GetDamageFromCollisonRelativeVelcoity(collision.relativeVelocity.magnitude)*
+        //Damage multiplier as AI speed is restricted. Change in future if needed.
+        float damage = GameManager.INSTANCE.GetDamageFromCollisonRelativeVelcoity(collision.relativeVelocity.magnitude) *
             AIManager.INSTANCE.damageMultiplier;
+
+        //If damge is done by this car then add score and apply damage to the opponent.    
         if (performedBySelf)
         {
             stats.AddScore(GameManager.INSTANCE.GetScoreFromDamage(damage));
@@ -307,6 +371,8 @@ public class AICar : MonoBehaviour
         }
         else
         {
+            //If damge is done by another contenstant then collect energy.
+            //The damge to health is updated by another contenstant.
             stats.CollectEnergy(GameManager.INSTANCE.GetEnergyFromDamage(damage));
         }
     }
@@ -325,13 +391,16 @@ public class AICar : MonoBehaviour
 
     private void AI()
     {
+        //If inside tornado do nothing
         if (insideTornado)
         {
             horizontalInput = 0;
             verticalInput = 0;
             return;
         }
+
         StuckingCheck();
+
         if (flipped)
         {
             Reset();
@@ -347,17 +416,12 @@ public class AICar : MonoBehaviour
                 DecideWayPoint();
             }
             MoveToWayPoint();
-            //We are not checking smart boundary for cases when car is reversing. It will fall.
+            //We are not checking smart boundary for cases when car is reversing. This is a serious issue so fix in future.
             SmartBoundaryDetection();
             SenseEnv();
-            if (obstacleFound)
-            {
-                horizontalInput = obstacleAvoidanceInput;
-            }
         }
     }
 
-    //Not handling Center sensor cases here.
     private void ReactToSense(Sensor sensor, RaycastHit hit, Vector3 sensorPos)
     {
         if (AIManager.INSTANCE.IsInvisibleBoundary(hit.collider))
@@ -368,10 +432,12 @@ public class AICar : MonoBehaviour
             }
             else
             {
+                //Slow down on invisible boundary
                 if (this.rigidbody.velocity.magnitude > AIManager.INSTANCE.invisibleBoundarySpeed)
                 {
                     this.carController.ApplyHandBrake();
                 }
+                //Update obstacle avoidance.
                 this.obstacleFound = true;
                 this.obstacleAvoidanceInput += GetObstacleAvoidanceInput(sensor, hit);
             }
@@ -386,13 +452,14 @@ public class AICar : MonoBehaviour
             }
             else
             {
+                //Update obstacle avoidance.
                 obstacleFound = true;
                 obstacleAvoidanceInput += GetObstacleAvoidanceInput(sensor, hit);
                 Debug.DrawLine(sensorPos, hit.point, Color.yellow);
             }
         }
-        //Can't shoot with angle sensors and when inside tornado
-        else if (sensor != Sensor.LEFT_ANGLE && sensor != Sensor.RIGHT_ANGLE && !insideTornado &&
+        //Will not shoot if detected by angle sensors.
+        else if (sensor != Sensor.LEFT_ANGLE && sensor != Sensor.RIGHT_ANGLE &&
             projectileShooter.CanShoot() && stats.IsEnergyFull() && AIManager.INSTANCE.IsShootable(hit.collider))
         {
             projectileShooter.Shoot();
@@ -418,14 +485,10 @@ public class AICar : MonoBehaviour
         float sensorLength = AIManager.INSTANCE.sensorLength;
         float sensorAngle = AIManager.INSTANCE.sensorAngle;
         Vector3 sensorPos = transform.position;
+        //Adjust sensor position according to car dimensions.
         sensorPos += transform.forward * carController.carDimensions.z / 2;
         sensorPos += transform.up * carController.carDimensions.y / 2;
-        //Clear if there is some input from the previous sense iteration.
-        obstacleFound = false;
-        obstacleAvoidanceInput = 0f;
         RaycastHit hit;
-
-        //Shotting is enabled only for Right, Left and Centre sensors not for Angluar ones.
 
         //Check right obstacles.
         //Front right sensor
@@ -463,7 +526,7 @@ public class AICar : MonoBehaviour
             ReactToSense(Sensor.CENTER, hit, sensorPos);
         }
         //Back sesnor
-        //Not tested properly.
+        //NOT TESTED PROPERLY
         sensorPos -= 2 * transform.forward * (carController.carDimensions.z / 2);
         if (Physics.Raycast(sensorPos, -transform.forward, out hit, sensorLength))
         {
@@ -478,6 +541,7 @@ public class AICar : MonoBehaviour
         }
     }
 
+    //A method to show the current way point selection in debug mode.
     private void DebugWaypointSelection()
     {
         Color color = Color.blue;
@@ -511,10 +575,12 @@ public class AICar : MonoBehaviour
         bool outsideSmartBoundary = IsOutsideSmartBoundary();
         if (outsideSmartBoundary)
         {
+            //Reverse first time when outside smart boundary.
             if (!wasOutsideSmartBoundary)
             {
                 StartReversing();
             }
+            //Slow down the speed.
             if (rigidbody.velocity.magnitude > AIManager.INSTANCE.smartBoundarySpeed)
             {
                 carController.ApplyHandBrake();
@@ -577,10 +643,12 @@ public class AICar : MonoBehaviour
         {
             reached = true;
         }
+        //The object which this car was chasing destroyed.
         else if (currWaypoint.containsTransform && currWaypoint.transform == null)
         {
             reached = true;
         }
+        //If not able to reach a static way point in a fixed amount of time.
         else if (currWaypoint.wayPointType != AIManager.WayPointType.PLAYER &&
             currWaypoint.wayPointType != AIManager.WayPointType.AI_CAR &&
             Time.time - lastWayPointTime > AIManager.INSTANCE.wayPointReachingThresholdInSecs)
@@ -593,6 +661,7 @@ public class AICar : MonoBehaviour
                     transform.position, currWaypoint.containsTransform ? currWaypoint.transform.position : currWaypoint.vector
                     ) <= AIManager.INSTANCE.wayPointDistanceThreshold;
         }
+        //Update the time if reached to the waypoint.
         if (reached)
         {
             lastWayPointTime = Time.time;
@@ -619,29 +688,6 @@ public class AICar : MonoBehaviour
             return;
         }
         currWaypoint = AIManager.INSTANCE.GetRandWayPoint(stats.displayName);
-        DebugWaypointSelection();
-    }
-
-    //Right now this mehtod is not used so can look to remove it in future.
-    private void MoveTowardsSafteyPoint()
-    {
-        //Try to collect health;
-        if (!stats.IsHealthFull() && TrySelectingHealthWaypoint())
-        {
-            return;
-        }
-        //Try to collect energy
-        if (!stats.IsEnergyFull() && TrySelectingEnergyWaypoint())
-        {
-            return;
-        }
-        //Try selecting Nitro.
-        if (TrySelectingNitroWaypoint())
-        {
-            return;
-        }
-        //Move to a safe place decided by AI manager
-        currWaypoint = AIManager.INSTANCE.GetSafteyPoint(stats.displayName);
         DebugWaypointSelection();
     }
 
@@ -683,6 +729,7 @@ public class AICar : MonoBehaviour
 
     private bool TrySelectingPlayerWaypoint()
     {
+        //Decide on the basis of a probability
         int chances = (int)(1 / AIManager.INSTANCE.playerAttackingProb);
         if (Random.Range(0, chances) != 0)
         {
@@ -700,6 +747,7 @@ public class AICar : MonoBehaviour
 
     private bool TrySelectingOtherAIWaypoint()
     {
+        //Decide on the basis of a probability
         int chances = (int)(1 / AIManager.INSTANCE.otherAIAttackcingProb);
         if (Random.Range(0, chances) == 0)
         {
